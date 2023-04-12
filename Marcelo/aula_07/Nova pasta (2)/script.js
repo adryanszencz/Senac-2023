8039201c9ecbfb55f281ff5a31b65736910403e5
adryanszencz <adryanszenczuk@gmail.com>
2023-04-12 19:52:43 -0300

    ..

--- Marcelo/aula_07/Nova pasta (2)/script.js
+++ Marcelo/aula_07/Nova pasta (2)/script.js
@@ -1,45 +1,153 @@
 function composto(name){
 
-        // Reactive Nonmetal
-        const corRN:'#e6f0ff';
-        // Noble Gas
-         const corNG: #ffebee ;
-        /* Alkali Metal*/
-        const corAM:'#DBF8FF';
-        /* Alkaline Earth Metal*/
-        const corAEM:'#ffebeb';
-        /* Metalloid*/
-        const corM:'#fef9e6';
-        /* Post-transition Metal*/
-        const corPTM:'#DCFAEB';
-        /* Transition Metal*/
-        const corTM:'#f5ecfd';
-        /* Lanthanide*/
-        const corL:'#E6f5ff';
-        /* Actinide*/
-        const corA:'#ffeadb';
-        /* Unknown Chemical Properties*/
-    --corUCP:#e9e9ec;
+    /* Reactive Nonmetal*/
+    let corRN = '#e6f0ff';
+    /* Noble Gas*/
+    let corNG = '#ffebee';
+    /* Alkali Metal*/
+    let corAM = '#DBF8FF';
+    /* Alkaline Earth Metal*/
+    let corAEM = '#ffebeb';
+    /* Metalloid*/
+    let corM = '#fef9e6';
+    /* Post-transition Metal*/
+    let corPTM = '#DCFAEB';
+    /* Transition Metal*/
+    let corTM = '#f5ecfd';
+    /* Lanthanide*/
+    let corL = '#E6f5ff';
+    /* Actinide*/
+    let corA = '#ffeadb';
+    /* Unknown Chemical Properties*/
+    let corUCP = '#e9e9ec';
+
     if(name == 'H'){
+
         let compostosname = 'Hydrogen';
-        let copostosigla = 'H';
+        let copostosigla = '(H)';
         let copostocategoria = 'Reactive Nonmetal';
         let copostodescriçao = 'Hydrogen is the most common element in the universe, making up about 75% of its mass.';
-        document.getElementById('namecoposto').innerHTML = compostosname;
-        document.getElementById('siglacoposto').innerHTML = copostosigla;
-        document.getElementById('categoriacoposto').innerHTML = copostocategoria;
-        document.getElementById('descriçaocoposto').innerHTML = copostodescriçao;
         
-    } 
-    if (name == 'He') {
-        let compostosname = 'Ipsum';
-        let copostosigla = 'IP';
-        let copostocategoria = 'Nãometal';
-        let copostodescriçao = 'IPSUM.';
+        let compostoSymbol = 'Symbol: H';
+        let compostoAtomic = 'Atomic mass: 1.00784 u';
+        let compostoDensity = 'Density: 0.07099 g/cm3';
+        let compostoMelting = 'Melting: -259.16 °C';
+        let compostoBoiling = 'Boiling: -252.87 °C';
+        let compostoDiscovered = 'Discovered by: Henry Cavendish';
+        let compostodiscovery = 'Year of discovery: 1766';
+
         document.getElementById('namecoposto').innerHTML = compostosname;
         document.getElementById('siglacoposto').innerHTML = copostosigla;
         document.getElementById('categoriacoposto').innerHTML = copostocategoria;
         document.getElementById('descriçaocoposto').innerHTML = copostodescriçao;
-        document.getElementById('descriçaocoposto').style.color=
+
+        document.getElementById('Symbolcomposto').innerHTML = compostoSymbol;
+        document.getElementById('Atomiccomposto').innerHTML = compostoAtomic;
+        document.getElementById('Densitycomposto').innerHTML = compostoDensity;
+        document.getElementById('Meltingcomposto').innerHTML = compostoMelting;
+        document.getElementById('Boilingcomposto').innerHTML = compostoBoiling;
+        document.getElementById('Discoveredcomposto').innerHTML = compostoDiscovered;
+        document.getElementById('discoverycomposto').innerHTML = compostodiscovery;
+        
+        document.getElementById('namecoposto').style.color = corRN;
+        document.getElementById('siglacoposto').style.color = corRN;
+        document.getElementById('categoriacoposto').style.color = corRN;
+        document.getElementById('descriçaocoposto').style.color = corRN;
+
+        document.getElementById('Symbolcomposto').style.color = corRN;
+        document.getElementById('Atomiccomposto').style.color = corRN;
+        document.getElementById('Densitycomposto').style.color = corRN;
+        document.getElementById('Meltingcomposto').style.color = corRN;
+        document.getElementById('Boilingcomposto').style.color = corRN;
+        document.getElementById('Discoveredcomposto').style.color = corRN;
+        document.getElementById('discoverycomposto').style.color = corRN;
+
+    } 
+
+    if(name == 'He'){
+
+        let compostosname = 'Helium';
+        let copostosigla = '(He)';
+        let copostocategoria = 'Noble Gas';
+        let copostodescriçao = 'Helium was discovered on the sun before it was found on the earth.';
+        
+        let compostoSymbol = 'Symbol: He';
+        let compostoAtomic = 'Atomic mass: 4.002602 u';
+        let compostoDensity = 'Density: 0.145 g/cm3';
+        let compostoMelting = 'Melting: -272.2 °C';
+        let compostoBoiling = 'Boiling: -268.93 °C';
+        let compostoDiscovered = 'Discovered by: William Ramsay, Norman Lockyer, Per Teodor Cleve, Pierre Janssen';
+        let compostodiscovery = 'Year of discovery: 1868';
+
+        document.getElementById('namecoposto').innerHTML = compostosname;
+        document.getElementById('siglacoposto').innerHTML = copostosigla;
+        document.getElementById('categoriacoposto').innerHTML = copostocategoria;
+        document.getElementById('descriçaocoposto').innerHTML = copostodescriçao;
+
+        document.getElementById('Symbolcomposto').innerHTML = compostoSymbol;
+        document.getElementById('Atomiccomposto').innerHTML = compostoAtomic;
+        document.getElementById('Densitycomposto').innerHTML = compostoDensity;
+        document.getElementById('Meltingcomposto').innerHTML = compostoMelting;
+        document.getElementById('Boilingcomposto').innerHTML = compostoBoiling;
+        document.getElementById('Discoveredcomposto').innerHTML = compostoDiscovered;
+        document.getElementById('discoverycomposto').innerHTML = compostodiscovery;
+        
+        document.getElementById('namecoposto').style.color = corNG;
+        document.getElementById('siglacoposto').style.color = corNG;
+        document.getElementById('categoriacoposto').style.color = corNG;
+        document.getElementById('descriçaocoposto').style.color = corNG;
+
+        document.getElementById('Symbolcomposto').style.color = corNG;
+        document.getElementById('Atomiccomposto').style.color = corNG;
+        document.getElementById('Densitycomposto').style.color = corNG;
+        document.getElementById('Meltingcomposto').style.color = corNG;
+        document.getElementById('Boilingcomposto').style.color = corNG;
+        document.getElementById('Discoveredcomposto').style.color = corNG;
+        document.getElementById('discoverycomposto').style.color = corNG;
+
     }
+
+    if(name == 'Li'){
+
+        let compostosname = 'Lithium';
+        let copostosigla = '(Li)';
+        let copostocategoria = 'Alkali Metal';
+        let copostodescriçao = 'Although it is a metal, Lithium it is soft enough to cut with a knife.';
+        
+        let compostoSymbol = 'Symbol: Li';
+        let compostoAtomic = 'Atomic mass: 6.941 u';
+        let compostoDensity = 'Density: 0.512 g/cm3';
+        let compostoMelting = 'Melting: 180.54 °C';
+        let compostoBoiling = 'Boiling: 1342 °C';
+        let compostoDiscovered = 'Discovered by: Johan August Arfwedson';
+        let compostodiscovery = 'Year of discovery: 1817';
+
+        document.getElementById('namecoposto').innerHTML = compostosname;
+        document.getElementById('siglacoposto').innerHTML = copostosigla;
+        document.getElementById('categoriacoposto').innerHTML = copostocategoria;
+        document.getElementById('descriçaocoposto').innerHTML = copostodescriçao;
+
+        document.getElementById('Symbolcomposto').innerHTML = compostoSymbol;
+        document.getElementById('Atomiccomposto').innerHTML = compostoAtomic;
+        document.getElementById('Densitycomposto').innerHTML = compostoDensity;
+        document.getElementById('Meltingcomposto').innerHTML = compostoMelting;
+        document.getElementById('Boilingcomposto').innerHTML = compostoBoiling;
+        document.getElementById('Discoveredcomposto').innerHTML = compostoDiscovered;
+        document.getElementById('discoverycomposto').innerHTML = compostodiscovery;
+        
+        document.getElementById('namecoposto').style.color = corAM;
+        document.getElementById('siglacoposto').style.color = corAM;
+        document.getElementById('categoriacoposto').style.color = corAM;
+        document.getElementById('descriçaocoposto').style.color = corAM;
+
+        document.getElementById('Symbolcomposto').style.color = corAM;
+        document.getElementById('Atomiccomposto').style.color = corAM;
+        document.getElementById('Densitycomposto').style.color = corAM;
+        document.getElementById('Meltingcomposto').style.color = corAM;
+        document.getElementById('Boilingcomposto').style.color = corAM;
+        document.getElementById('Discoveredcomposto').style.color = corAM;
+        document.getElementById('discoverycomposto').style.color = corAM;
+
+    }
+
 }
