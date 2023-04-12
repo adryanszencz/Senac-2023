ec41c474d75da0f2b3324f467e7ec3a261116080
adryanszencz <adryanszenczuk@gmail.com>
2023-04-11 21:27:24 -0300

    ..

---
+++ Bruno/Aula-02/script.js
@@ -0,0 +1,6 @@
+const primeiroNme = prompt('Digital o primeiro nome:')
+const segundoNome = prompt('Digital o segundo nome:')
+const campoEstudo = prompt('Digital o campo estudo:')
+const anoNascimento = prompt('Digital o ano nascimento:')
+
+alert('Nome Completo: ' +primeiroNme+ ' ' +segundoNome)
