85eb01b42ee4a28eee524852eed9113fc8c7deeb
adryanszencz <adryanszenczuk@gmail.com>
2023-04-19 17:28:44 -0300

    ..

--- Marcelo/aula_09/js/script.js
+++ Marcelo/aula_09/js/script.js
@@ -1,8 +1,36 @@
 
-function entrar(){
-    var email = document.getElementById('email');
-    var senha = document.getElementById('senha');
-    if(email.value == "adryan"){
-        alert('entrou')
+function botao(valor){
+
+    let emailUsuario
+    let senhaUsuario
+
+
+    if(valor == '1'){
+        alert(`${emailUsuario}`)
+        email = document.getElementById('email').value
+        senha = document.getElementById('senha').value
+        if(email == emailUsuario && senha == senhaUsuario){
+            alert('entrou')
+
+        }
+        
     }
+    if(valor == '2'){
+        let comfimarSenhaUsuario
+        emailCadastro = document.getElementById('emailCadastro').value
+        senhaCadastro = document.getElementById('senhaCadastro').value
+        comfimarSenhaUsuario = document.getElementById('comfimarSenhaUsuario').value 
+        if(senhaCadastro == comfimarSenhaUsuario){  
+            alert('Realizado')
+            window.history.go(-1)  
+            alert(`${senhaUsuario}`) 
+        }else{
+            alert('algo de errado')
+        }  
+
+        emailUsuario = emailCadastro
+        senhaUsuario = senhaUsuario
+        
+    }
+
 }
